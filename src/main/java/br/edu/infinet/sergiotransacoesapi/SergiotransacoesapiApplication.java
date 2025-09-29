0bc731761757a5c8b094ccfb9e8cdd174bb88297
sergiodiogoti <sergiodiogoti@gmail.com>
2025-09-29 20:33:14 -0300

    Estruturação do projeto

---
+++ src/main/java/br/edu/infinet/sergiotransacoesapi/SergiotransacoesapiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infinet.sergiotransacoesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SergiotransacoesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SergiotransacoesapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infinet/sergiotransacoesapi/SergiotransacoesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infinet.sergiotransacoesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SergiotransacoesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
